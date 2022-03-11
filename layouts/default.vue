d0a0d91dc4c72a8dc09a09524be47818e66c18dc
mattvesta <matt.vesta@aceyus.com>
2022-03-11 16:19:42 -0500

    Initial Commit

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <core-app-bar />
+  </v-app>
+</template>
+<script>
+
+export default ({
+  name: 'App',
+  components: {
+    // CoreDrawer: () => import('@/components/core/Drawer'),
+    // CoreFooter: () => import('@/components/core/Footer'),
+    CoreAppBar: () => import('@/components/core/AppBar')
+    // CoreView: () => import('@/components/core/View')
+  }
+})
+</script>
+  <style lang="sass">
+
+  </style>
---
+++ plugins/buttercms.js
@@ -0,0 +1,2 @@
+const Butter = require('buttercms')
+export const butter = Butter('cf08cde69a9dd6f7b776465ed89397a80588e992')
